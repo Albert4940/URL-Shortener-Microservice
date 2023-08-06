e5b9f036830bf2465b2bc3092eb4b77c23b8b121
Albert4940 <dorcealbertmary@gmail.com>
2023-08-06 16:17:19 -0400

    verify a submitted URL.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 var path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const dns = require('dns');
+const {URL} = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -26,6 +28,27 @@ const urlSchema = new mongoose.Schema({
 
 const Url = mongoose.model('Url',urlSchema);
 
+const createAndSaveUrl = data => {
+    //const url = 
+}
+
+//function to check url with promise
+const checkEmailPromise = (dns, url) => {
+    const urlObject = new URL(url);
+     const hostname = urlObject.hostname;
+   return new Promise((resolve,reject) => {
+     dns.lookup(hostname, (error, address, family) => {
+       // if an error occurs, eg. the hostname is incorrect!
+       if (error) {
+         reject({error:error.message})
+       } else {
+         // if no error exists
+          resolve({address:address})
+       }
+     });
+   })
+ }
+  
 app.use(bodyParser.urlencoded({exetended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,9 +57,25 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.post('/api/shorturl', (req, res) => {
+app.post('/api/shorturl', async (req, res) => {
     const {url} = req.body;
-    res.json({url:url});
+    
+    let respons = {};
+    let isHostNameValid = false;
+    try{
+       isHostNameValid = await checkEmailPromise(dns,url);
+    }catch(error){
+      isHostNameValid = error;
+    }
+    isHostNameValid = isHostNameValid.address ? true : false;
+    
+      if(isHostNameValid){
+        respons = {"original_url":url,"short_url":2}
+      }else{
+        respons = {"error":"Invalid Hostname"}
+      }
+  //  console.log(reponse)
+    res.json(respons);
 })
 
 app.listen(port, function(){
