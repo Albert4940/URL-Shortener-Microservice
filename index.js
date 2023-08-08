a43d9cf6c933e18b41c7343fa2bd3783ba372948
Albert4940 <dorcealbertmary@gmail.com>
2023-08-08 00:00:36 -0400

    Check if a document already exist

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
-});
+}).then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
+    console.error('Database connection error');
+  });;
 
 const urlSchema = new mongoose.Schema({
     original_url:{
@@ -28,11 +33,57 @@ const urlSchema = new mongoose.Schema({
 
 const Url = mongoose.model('Url',urlSchema);
 
-const createAndSaveUrl = data => {
-    //const url = 
+const findAllUrl = async () => {
+   const response = await Url.find({});
+   return response;
+}
+
+const isExist = (urls,urlSubmitted) => {
+  const newUrls = [...urls];
+  if(newUrls.length == 0)
+    return false;
+
+  return newUrls.find(url => url.original_url === urlSubmitted) == undefined ? false : true;
+
+}
+
+const findOneUrl = async url=> {
+  let response = {};
+  try{
+    response = await Url.findOne({original_url: url});
+  }catch(err){
+    response = err;
+  }
+  return response;
+}
+
+const createAndSaveUrl = async url => {
+    //
+    const urls = await findAllUrl();
+    const nbUrl = urls.length;
+    const existUrl = await findOneUrl(url);
+
+    if(existUrl != null)
+      return existUrl;
+
+    const urlModel = new Url({
+        original_url:url,
+        short_url:nbUrl + 1
+    })
+    let response = {}
+
+    try{
+        response = await urlModel.save();
+    }catch(err){
+        response = err;
+    }
+    
+    return response;
+
 }
 
 //function to check url with promise
+//check url with out http
 const checkEmailPromise = (dns, url) => {
     const urlObject = new URL(url);
      const hostname = urlObject.hostname;
@@ -60,22 +111,27 @@ app.get('/', (req,res) => {
 app.post('/api/shorturl', async (req, res) => {
     const {url} = req.body;
     
-    let respons = {};
+    let response = {};
+    //refactoring create function
+    //Add try catch
     let isHostNameValid = false;
     try{
        isHostNameValid = await checkEmailPromise(dns,url);
     }catch(error){
       isHostNameValid = error;
     }
+
     isHostNameValid = isHostNameValid.address ? true : false;
     
-      if(isHostNameValid){
-        respons = {"original_url":url,"short_url":2}
+      if(isHostNameValid){   
+        const data = await createAndSaveUrl(url);
+        const {original_url,short_url} = data;        
+        response = {"original_url":original_url,"short_url":short_url}
       }else{
-        respons = {"error":"Invalid Hostname"}
+        response = {"error":"invalid url"}
       }
   //  console.log(reponse)
-    res.json(respons);
+    res.json(response);
 })
 
 app.listen(port, function(){
